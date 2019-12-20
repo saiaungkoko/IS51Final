d8ce85d6ae85c7db34456bd29446cb501c5ec60f
saiaungkoko <saiaungkoko@mail.fresnostate.edu>
2019-12-19 18:50:40 -0800

    finish final Exam

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,12 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 import { Subject } from 'rxjs';
+
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: IUser = {
+    username: null,
+    password: null
+  };
 
   constructor(private router: Router, private toastService: ToastService) {
   }
@@ -15,5 +25,20 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
 
   }
+
+  login(user: IUser) {
+    const presentUser = { username: 'Sai', password: 'sai123' };
+
+    if (user.username != null && user.password != null && user.username !== '' && user.password !== '') {
+      if (user.username === presentUser.username && user.password === presentUser.password) {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.router.navigate(['cart']);
+      } else {
+        this.toastService.showToast('danger', 5000, 'Invalid credential');
+      }
+    } else {
+      this.toastService.showToast('danger', 5000, 'Put Username and Passwords');
+    }
+    }
 
 }
